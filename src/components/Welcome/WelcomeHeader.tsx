c60d17442f0f70db97df67f424ee06c4bdfd3f52
Brett Tomita <bretttomita@gmail.com>
2025-05-21 12:50:50 -0700

    Welcome Customization

--- src/components/Welcome/WelcomeHeader.tsx
+++ src/components/Welcome/WelcomeHeader.tsx
@@ -47,7 +47,7 @@ export default function WelcomeHeader() {
             >
                 <SpaceBetween size="xl">
                     <Box fontSize="display-l" fontWeight="bold">
-                        Demo Application Experience
+                        Cerca Labs HealthScribe Demo
                     </Box>
                     <Box fontSize="display-l">Powered by AWS HealthScribe</Box>
                 </SpaceBetween>
